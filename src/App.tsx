22bb49e8026e83d9d9d214f3940f853a5facdf47
Janaina Oliveira <janainaon1@gmail.com>
2020-08-11 22:16:29 -0300

    finalizado toast

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 import GlobalStyles from './styles/global';
 import SignIn from './pages/SignIn';
+// import SignUp from './pages/SignUp';
+
+import AppProvider from './hooks';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AppProvider>
+      <SignIn />
+    </AppProvider>
+
     <GlobalStyles />
   </>
 );
